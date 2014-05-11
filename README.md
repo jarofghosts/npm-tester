7b1df4f45ac2d768e9bd82aaee104e3cbcd863da
jesse keane <me@jessekeane.me>
2014-05-10 17:17:29 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+npm-tester
+====
+
+derp
---
+++ index.js
@@ -0,0 +1,66 @@
+var os = require('os')
+
+var request = require('hyperquest')
+  , through = require('through')
+  , tap = require('tap-parser')
+
+module.exports = tester
+
+function tester(server) {
+  var parser = tap(submit)
+    , start = Date.now()
+    , results
+
+  var stream = through(write, noop)
+
+  stream.submit = end
+
+  return stream
+
+  function end() {
+    parser.end()
+  }
+
+  function write(data) {
+    parser.write(data)
+
+    stream.queue(data)
+  }
+
+  function submit(results) {
+    var os_data = {
+        platform: os.platform()
+      , type: os.type()
+      , arch: os.arch()
+      , release: os.release()
+      , totalmem: os.totalmem()
+      , freemem: os.freemem()
+      , cpus: os.cpus()
+    }
+
+    var result_data = {
+        passed: results.ok
+      , os: os_data
+      , results: results
+      , node: process.version
+      , elapsed: Date.now() - start
+    }
+
+    request.post(server)
+      .on('error', onerror)
+      .on('end', onend)
+      .end(JSON.stringify(result_data))
+
+    function onend() {
+      process.stderr.write('submitted results!\n')
+      stream.queue(null)
+      process.exit(+(!results.ok))
+    }
+  }
+}
+
+function onerror(err) {
+  process.stderr.write(err)
+}
+
+function noop() {}
